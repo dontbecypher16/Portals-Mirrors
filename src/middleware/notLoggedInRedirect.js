d09a3b41a9ab422754a1120773566a9ecd1c6197
Richard Dixon <rvictordixon@gmail.com>
2021-10-25 07:04:48 -0400

    repeat

--- src/middleware/notLoggedInRedirect.js
+++ src/middleware/notLoggedInRedirect.js
@@ -3,7 +3,7 @@ const User = require('../models/userSchema')
 module.exports = (req, res, next) => {
     User.findById(req.session.userId, (error, user) => {
         if (!user) {
-            res.redirect('/auth/login');
+          return  res.redirect('/auth/login');
         }
 
         next()
