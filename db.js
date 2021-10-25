510c5dcf55ca2ca2f9e5f8abf77e57863306c009
Richard Dixon <rvictordixon@gmail.com>
2021-10-25 06:50:29 -0400

    repeat

--- db.js
+++ db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose')
 const uri = process.env.MONGODB_URI;
 
 module.exports = function() {
-   mongoose.connect(uri,
- {useNewUrlParser: true,
- useUnifiedTopology: true }, (err) => {
+   mongoose.connect(uri, (err) => {
     if(err)  {
         console.log(err.message)
     } else {
